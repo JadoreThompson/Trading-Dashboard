8e8bfca744f9cadacce029edb9d268103f7f5247
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-06-25 20:00:01 +0100

    loaded todays growth, 7d growth and currencyin views.py

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 from views import views
+import os
+from dotenv import load_dotenv
+import MetaTrader5 as mt5
 
+# Load environment variables
+load_dotenv('icmarkets.env')
+
+# Retrieve login credentials
+login = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
+server = os.getenv('SERVER')
+
+# logging in
+authorised = mt5.initialize(login=int(login), password=password, server=server)
+
+if authorised:
+    print("Y")
 
 app = Flask(__name__)
 app.register_blueprint(views)
--- views.py
+++ views.py
@@ -8,16 +8,6 @@ from datetime import datetime, date, timedelta
 
 views = Blueprint('views', __name__)
 
-mt5.initialize()
-credentials = {
-    'login': int(os.getenv('LOGIN')),
-    'password': os.getenv('PASSWORD'),
-    'server': os.getenv('SERVER')
-}
-
-from_date = datetime(1970,6,1)
-today = date.today()
-
 
 @views.route('/')
 def index():
@@ -26,5 +16,59 @@ def index():
 
 @views.route('/dashboard')
 def dashboard():
+    def total_profit():
+        ttl_profit = mt5.account_info()
 
-    return render_template('dashboard.html')
+        return ttl_profit
+
+    ttl_profit = total_profit()
+    if ttl_profit is None:
+        ttl_profit = 0
+    else:
+        ttl_profit = ttl_profit[10]
+        print(ttl_profit)
+
+    def weekly_profit():
+        today = datetime.now()
+        past_week = today - timedelta(weeks=1)
+        deals = mt5.history_deals_get(past_week, today)
+        formatted_deals = []
+
+        for deal in deals:
+            if deal[4] == 0:
+                formatted_deal = {
+                    "ticket": deal.ticket,
+                    "order": deal.order,
+                    "time": deal.time,
+                    "time_msc": deal.time_msc,
+                    "type": deal.type,
+                    "entry": deal.entry,
+                    "magic": deal.magic,
+                    "position_id": deal.position_id,
+                    "reason": deal.reason,
+                    "volume": deal.volume,
+                    "price": deal.price,
+                    "commission": deal.commission,
+                    "swap": deal.swap,
+                    "profit": deal.profit,
+                    "fee": deal.fee,
+                    "symbol": deal.symbol,
+                    "comment": deal.comment,
+                    "external_id": deal.external_id
+                }
+                formatted_deals.append(formatted_deal)
+
+        return formatted_deals
+
+    w_profit = weekly_profit()
+    prof = 0
+    for profit in w_profit:
+        print("Profit: ", profit['profit'])
+        prof = round(prof + profit['profit'], 2)
+
+    currency = mt5.account_info()
+    currency = currency[26]
+
+
+    return render_template('dashboard.html', ttl_profit=ttl_profit, w_profit=prof,
+                           currency=currency)
